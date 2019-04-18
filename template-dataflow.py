34ad43fd14cec6fa24134201f87c2d24be0268fb
Loic-C <49689982+Loic-C@users.noreply.github.com>
2019-04-18 11:54:57 +0200

    Update template-dataflow.py

--- template-dataflow.py
+++ template-dataflow.py
@@ -1,29 +1,53 @@
 from __future__ import absolute_import
+import argparse
 
-import csv
 import apache_beam as beam
 from apache_beam.io import ReadFromText
 from apache_beam.io import WriteToText
 from apache_beam.metrics.metric import MetricsFilter
-from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, SetupOptions
 
+""" First step : 
+create template on dataflow with the comand line -> python template.py --runner DataflowRunner --project $PROJECT --staging_location gs://$BUCKET/staging --temp_location gs://$BUCKET/temp --
+template_location gs://$BUCKET/templates/$TemplateName """
 
-class UploadOptions(PipelineOptions):
+class CustomPipelineOptions(PipelineOptions):
+    """
+    Runtime Parameters given during template execution
+    path and organization parameters are necessary for execution of pipeline
+    campaign is optional for committing to bigquery
+    """
     @classmethod
     def _add_argparse_args(cls, parser):
         parser.add_value_provider_argument(
-            '--input',
-            default='gs://some/input.csv',
+            '--path',
+            type=str,
             help='Path of the file to read from')
         parser.add_value_provider_argument(
             '--output',
-            required=True,
-            help='Output file to write results to.')
+            type=str,
+            help='Source name')
 
-pipeline_options = PipelineOptions(['--output', 'gs://some/output'])
-p = beam.Pipeline(options=pipeline_options)
-upload_options = pipeline_options.view_as(UploadOptions)
+def run(argv=None):
+    parser = argparse.ArgumentParser()
+    known_args, pipeline_args = parser.parse_known_args(argv)
 
-(p
-    | 'read' >> beam.io.Read(upload_options.input)
-    | 'Write' >> beam.io.WriteToText(upload_options.output, file_name_suffix='.csv'))
+    global cloud_options
+    global custom_options
+
+    pipeline_options = PipelineOptions(pipeline_args)
+    cloud_options = pipeline_options.view_as(GoogleCloudOptions)
+    custom_options = pipeline_options.view_as(CustomPipelineOptions)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+
+    p = beam.Pipeline(options=pipeline_options)
+
+    init_data = (p
+                        | 'Hello World' >> beam.Create(['Hello World'])
+                 )
+
+    result = p.run()
+    # result.wait_until_finish
+
+if __name__ == '__main__':
+    run()
